bd81d7a9c95c58bc682922c744a66bbf91a311a9
kristina <kristinadinellari@gmail.com>
2019-07-31 12:10:31 +0200

    add show route

--- routes/articles.js
+++ routes/articles.js
@@ -1,21 +1,25 @@
 const express = require('express')
 const router = express.Router()
-const articleController = require('../articleController')
+const articlesController = require('../articlesController')
 
 router.get('/', (req, res) =>{
-    (new articleController(req, res)).list()
+    (new articlesController(req, res)).list()
+})
+
+router.get('/:id', (req, res) =>{
+    (new articlesController(req, res)).show()
 })
 
 router.post('/', (req, res) => {
-   (new articleController(req, res)).add()
+   (new articlesController(req, res)).add()
 })
 
 router.put('/:id', (req,res) => {
-    (new articleController(req, res)).update()
+    (new articlesController(req, res)).update()
 })
 
 router.delete('/:id', (req,res) => {
-   (new articleController(req, res)).delete()
+   (new articlesController(req, res)).delete()
 })
 
 module.exports = router
